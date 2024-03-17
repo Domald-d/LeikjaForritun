a3aa869702944ed358b9fca9c49c0749cc486262
Domald <78101890+Domald-d@users.noreply.github.com>
2024-03-17 14:17:24 +0000

    Update DifficultyButton.cs

--- Verkefni1/scripts/DifficultyButton.cs
+++ Verkefni1/scripts/DifficultyButton.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+//private og public breytur fyrir erfiðleika stig á tökkum
     private Button button;
     private GameManager gameManager;
     public int difficulty;
     // Start is called before the first frame update
     void Start()
     {
+    //náum í takka component
+    // tengjum gamemanager við skrá
         button = GetComponent<Button>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         button.onClick.AddListener(setDifficulty);
@@ -24,6 +27,7 @@ public class DifficultyButton : MonoBehaviour
 
     void setDifficulty()
     {
+    //void fall til að setja erfiðleika stig leiks
         gameManager.StartGame(difficulty);
     }
 }
